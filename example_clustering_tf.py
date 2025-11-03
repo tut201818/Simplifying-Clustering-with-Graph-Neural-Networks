2250ac59b5c0b7b5f88afde2e236a9bcf9ec9971
tut201818 <153586530+tut201818@users.noreply.github.com>
2025-11-04 00:55:27 +0900

    Update example_clustering_tf.py

--- example_clustering_tf.py
+++ example_clustering_tf.py
@@ -11,12 +11,13 @@ from tensorflow.keras.models import Model
 
 from spektral.utils.sparse import sp_matrix_to_sp_tensor
 #from spektral.datasets import Citation, DBLP
-from spektral.datasets import Citation#
+from spektral.datasets import Citation #
 from spektral.utils.convolution import normalized_laplacian
-from spektral.layers import GCNConv
-#from spektral.layers.pooling import JustBalancePool
+#from spektral.layers import GCNConv
+from spektral.layers.convolutional import GCNConv #
+from spektral.layers.pooling import JustBalancePool
 #from spektral.layers import JustBalancePool
-from just_balance import just_balance_pool
+#from just_balance import just_balance_pool
 
 from eval_metrics import eval_metrics
 
