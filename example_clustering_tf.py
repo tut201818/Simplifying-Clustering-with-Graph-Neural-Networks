36ee2fed4cc2b1d98971b216a48c888082f6061b
tut201818 <153586530+tut201818@users.noreply.github.com>
2025-11-04 14:11:33 +0900

    Update example_clustering_tf.py

--- example_clustering_tf.py
+++ example_clustering_tf.py
@@ -14,9 +14,9 @@ from spektral.utils.sparse import sp_matrix_to_sp_tensor
 from spektral.datasets import Citation #
 from spektral.utils.convolution import normalized_laplacian
 #from spektral.layers import GCNConv
-from spektral.layers.convolutional import gcn_conv
-#from spektral.layers.pooling import JustBalancePool
-from spektral.layers.pooling import Just_balance_pool
+from spektral.layers.convolutional import GCNConv
+from spektral.layers.pooling import JustBalancePool
+#from spektral.layers.pooling import Just_balance_pool
 
 from eval_metrics import eval_metrics
 
